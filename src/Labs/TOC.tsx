180f138a4f813a507b03c46e92a31c3c987bd049
Thomas McIninch <mcininch.t@northeastern.edu>
2025-02-04 20:55:33 -0500

    add name and git repo

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -2,25 +2,27 @@ import Nav from "react-bootstrap/Nav";
 // import { Link } from "react-router";
 
 export default function TOC() {
- return (
-   <Nav variant="pills">
-     <Nav.Item>
-       <Nav.Link href="#/Labs">Labs</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="https://github.com/DappleOfDawn">My GitHub</Nav.Link>
-     </Nav.Item>
-   </Nav>
+  return (
+    <Nav variant="pills">
+      <Nav.Item>
+        <Nav.Link href="#/Labs">Labs</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="https://github.com/DappleOfDawn/kambaz-react-web-app">
+          My GitHub
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
 );}
